feed56c1422da0a5d59b5f66cafc2235a23a16d3
Joe Kuczek <jkuczek@iastate.edu>
2017-10-22 16:41:15 -0500

    make the contact page actually work

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -1,37 +1,64 @@
+$('#submit').click(function(){
+	// list of error messages
+	var error_messages = {
+		email_require: 'Email is a required field.',
+		message_require: 'Message is a required field.',
+		name_require: 'Name is a required field.',
+		email_invalid: 'Reply-to email is invalid'
+	};
 
-(function($) { 
-	"use strict";
-jQuery(document).ready(function(){
-	$('#cform').submit(function(){
+	// grab the data
+	var data = {
+		"Name"      : $('#name').val(),
+		"Reply-To"  : $('#email').val(),
+		"Message"   : $('#comments').val(),
+	};
 
-		var action = $(this).attr('action');
+	// validate the form
+	var error = validate(data);
 
-		$("#message").slideUp(750,function() {
-		$('#message').hide();
+	if(error != null){
+		$('#message').attr('class', 'alert alert-danger');
+		$('#message').show();
+		$('#message').html(error_messages[error]);
+	}else{
+		send_email(data);
+	}// end if there is an error
 
- 		$('#submit')
-			.before('')
-			.attr('disabled','disabled');
-
-		$.post(action, {
-			name: $('#name').val(),
-			email: $('#email').val(),
-			comments: $('#comments').val(),
-		},
-			function(data){
-				document.getElementById('message').innerHTML = data;
-				$('#message').slideDown('slow');
-				$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
-				$('#submit').removeAttr('disabled');
-				if(data.match('success') != null) $('#cform').slideUp('slow');
-			}
-		);
-
-		});
-
-		return false;
-
-	});
-
+	return false;
 });
-}(jQuery));
+
+function validate(form_data){
+	// validate the contact form
+	if(form_data["Name"] == '')
+		return 'name_require';
+	if(form_data["Reply-To"] == '')
+		return 'email_require';
+	if(form_data["Message"] == '')
+		return 'message_require';
+	if(!validateEmail(form_data["Reply-To"]))
+		return 'email_invalid';
+	return null;
+}// end function for validating form
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}// end function validateEmail
+
+function send_email(form_data){
+	$.ajax({
+		url: "https://formspree.io/joe.kuczek@gmail.com", 
+		method: "POST",
+		data: form_data,
+		dataType: "json",
+		success: function(data){
+			$('#message').attr('class', 'alert alert-success');
+			$('#message').html("<strong>Success!</strong> Message has been sent, I'll be in touch with you shortly.");
+			$('#message').slideDown('slow');
+			$('#cform img.contact-loader').fadeOut('slow',function(){ $(this).remove() });
+			$('#submit').removeAttr('disabled');
+			$('#cform').slideUp('slow');
+		}// end success function
+	});
+}// end function for sending the email
